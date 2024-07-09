ff41ca64ffdb3715615382a9b43447e42a84f196
guyhaliva123 <116512226+guyhaliva123@users.noreply.github.com>
2024-07-09 11:50:56 +0300

    added flask server

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 
+
 function App() {
+
+  const [data, setData] = useState([{}])
+
+
+  useEffect(() => {
+    fetch("/users").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data);
+      }
+    )
+  }, [])
   return (
     <Router>
       <div className="App">
