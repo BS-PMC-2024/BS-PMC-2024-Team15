c1444bf25446638b65ef639a5ffbf1373a564b05
Guyaloosh <guyaloosh@gmail.com>
2024-08-09 10:51:56 +0300

    BSPMS2415-233

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,22 +2,23 @@ import React, { useState } from 'react';
 import '../ComponentsCss/Sidebar.css';
 
 
-const Sidebar = ({ scrollToCalendar, scrollToEvents, toggleAIAssistant, scrollToStatistics,scrollToCourses }) => {
+const Sidebar = ({ scrollToCalendar, scrollToEvents, toggleAIAssistant, scrollToStatistics, scrollToCourses }) => {
     // Sidebar components Scrolling functions
     const handleScrollToStatistics = () => {
-        scrollToStatistics(); 
+        scrollToStatistics();
     };
-    
+
     return (
         <>
             <aside className="sidebar">
                 <ul>
 
-                <a className="sidebar-btn" onClick={scrollToCalendar}><li><i class="fa-solid fa-calendar"></i>Calendar</li></a>
-                <a className="sidebar-btn" onClick={scrollToEvents}><li><i class="fa-solid fa-calendar-plus"></i>Events</li></a>
-                <a className="sidebar-btn" onClick={scrollToCourses}><li><i class="fa-solid fa-calendar"></i>Courses</li></a>
-                <a className="sidebar-btn" onClick={handleScrollToStatistics}><li><i class="fa-solid fa-calendar-plus"></i>Statistics</li></a>
-                <a className="sidebar-btn" onClick={toggleAIAssistant}><li><i class="fa-solid fa-calendar-plus"></i>Assistant Chat</li></a>
+                    <a className="sidebar-btn" onClick={scrollToCalendar}><li><i class="fa-solid fa-calendar"></i>Calendar</li></a>
+                    <a className="sidebar-btn" onClick={scrollToEvents}><li><i class="fa-solid fa-calendar-plus"></i>Events</li></a>
+                    <a className="sidebar-btn" onClick={scrollToCourses}><li><i class="fa-solid fa-calendar"></i>Courses</li></a>
+                    <a className="sidebar-btn" onClick={handleScrollToStatistics}><li><i class="fa-solid fa-calendar-plus"></i>Statistics</li></a>
+                    <a className="sidebar-btn" onClick={toggleAIAssistant}><li><i class="fa-solid fa-calendar-plus"></i>Assistant Chat</li></a>
+
 
                 </ul>
             </aside>
