57aced2bf176d0bba166435e297c843ee2575737
victor <victor@biblecollege.co.il>
2024-08-09 17:01:36 +0300

    minor fix

--- src/Components/RankForm.jsx
+++ src/Components/RankForm.jsx
@@ -14,7 +14,7 @@ const RankForm = ({ event, onClose, onSave }) => {
         <div className="modal-background">
             <div className="modal-content">
                 <h2>Rank {event.title} task</h2>
-                <p>Here you can rank your efficiefsdfsdncy in this assignment from 0 - 10</p>
+                <p>Here you can rank your efficiency in this assignment from 0 - 10</p>
                 <form onSubmit={handleSubmit}>
                     <div className="rating-options">
                         {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
